20a7170e9f4bbe74a6066caad276567d0e1b3155
Darwin G <dgarcia26@cuc.edu.co>
2023-05-29 18:44:03 -0500

    cambio 2

--- src/database.js
+++ src/database.js
@@ -1,13 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAyOKzYtIs-XUjhNd4yEspKg48nOITVeHQ",
   authDomain: "proyecto-bp-2023.firebaseapp.com",
   projectId: "proyecto-bp-2023",
   storageBucket: "proyecto-bp-2023.appspot.com",
   messagingSenderId: "312125058207",
-  appId: "1:312125058207:web:c0874487baecfbc26c86aa"
+  appId: "1:312125058207:web:bcda0265b0cd0c936c86aa"
 };
 
 // Initialize Firebase
